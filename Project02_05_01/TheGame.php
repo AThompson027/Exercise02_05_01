2e2c3c3d81185013f080975a0192b0b960856257
Unknown <AThompson027@west-mec.org>
2018-10-17 15:24:58 -0700

    More CSS + Fixed bug
    Added more CSS and fixed the error message bug

--- Project02_05_01/TheGame.php
+++ Project02_05_01/TheGame.php
@@ -1,11 +1,13 @@
 <html>
 <head>
     <script src="modernizr.custom.65897.js"></script>
-    <link rel="stylesheet" href="game.css">
+    <link href="game.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kodchasan" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Kodchasan|Mali" rel="stylesheet">
+    
 </head>
 <body>
-    <h2>THE GAME</h2>
+    <h2>The Game</h2>
     <?php
     //global variable
     $errormsg = "";
@@ -14,7 +16,7 @@
     // if the directory is a directory and if all the fields are empty then it will display an error message
     if(is_dir($dir)){
         if(empty($_POST['username']) || empty($_POST['password']) || empty($_POST['name']) || empty($_POST['screenname']) || empty($_POST['age']))  {
-            $errormsg .= "The required fields are left blank<br>\n";
+            $errormsg .= "<h3>The required fields are left blank</h3><br>\n";
         }
         // else will take off malicious slashes in data and also and open the file
         else {
@@ -33,24 +35,24 @@
             $filehandle = fopen($filename, "ab");
             // if the file is not open then there is an error message indicating that the file was failed to be created
             if($filehandle === false){
-                $errormsg .= "There was an error creating $filename<br>\n";
+                $errormsg .= "<h3>There was an error creating $filename</h3><br>\n";
             }
             //else will lock the file and then if there is info in the file it will indicate that is was successful
             else{
                 if(flock($filehandle, LOCK_EX)) {
                     if(fwrite($filehandle, $string) > 0){
-                        $errormsg .= "Successfully wrote to $filename<br>\n";
+                        $errormsg .= "<h4>Successfully wrote to $filename</h4><br>\n";
                     }
                     // else will indicate failure
                     else{
-                        $errormsg .= "There was an error writing to $filename<br>\n";
+                        $errormsg .= "<h3>There was an error writing to $filename</h3><br>\n";
                     }
                     // this unlocks the file
                     flock($filehandle, LOCK_UN);
                 }
                 // if the file does not lock then is will show an error
                 else{
-                    $errormsg .= "There was an error locking $filename<br>\n";
+                    $errormsg .= "<h3>There was an error locking $filename</h3><br>\n";
                 }
                 // closes the file
                 fclose($filehandle);
@@ -59,21 +61,25 @@
     }
     // else will echo an error indicating that the directory does not exist, but it will create the directory for the user
     else{
-        echo "Directory does not exist. Will create the file when good input is submitted\n";
+        echo "<h3>Directory does not exist. Will create the file when good input is submitted</h3>\n";
         mkdir($dir);
         chmod($dir,0757);//setting up permissions of the directory. it will go to 666 in my sandbox, unfortunatly.
+    }
+    // if the fields in the form is empty when you press the submit button then it will display the error
+    if(!empty($_POST['submit'])){
+        echo $errormsg;
     }
     ?>
        
 <!--       This is the form!!-->
         <form action="TheGame.php" method="post">
-            <label for="username">Username:<br></label><input type="text" id="username" name="username"><br>
-            <label for="password">Password:<br></label><input type="password" id="password" name="password"><br>
-            <label for="name">Full Name:<br></label><input type="text" id="name" name="name"><br>
-            <label for="email">Email:<br></label><input type="text" id="email" name="email"><br>
-            <label for="age">Age:<br></label><input type="number" id="age" name="age"><br>
-            <label for="screenname">Screenname:<br></label><input type="text" id="screenname" name="screenname"><br>
-            <label for="comments">Comments:<br></label><textarea id="comments" name="comments"></textarea><br>
+            <label for="username">Username</label><input type="text" id="username" name="username"><br>
+            <label for="password">Password</label><input type="password" id="password" name="password"><br>
+            <label for="name">Full Name</label><input type="text" id="name" name="name"><br>
+            <label for="email">Email</label><input type="text" id="email" name="email"><br>
+            <label for="age">Age</label><input type="number" id="age" name="age"><br>
+            <label for="screenname">Screenname</label><input type="text" id="screenname" name="screenname"><br>
+            <label for="comments">Comments</label><textarea id="comments" name="comments"></textarea><br>
             <input name="submit" value="submit" type="submit">
         </form>
 </body>
